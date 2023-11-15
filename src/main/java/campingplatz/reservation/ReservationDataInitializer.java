3420d78b79c9aff59ce27a4657492bf7584cef00
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-11-15 23:29:33 +0100

    removed copied code/comments

--- src/main/java/campingplatz/reservation/ReservationDataInitializer.java
+++ src/main/java/campingplatz/reservation/ReservationDataInitializer.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2013-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package campingplatz.reservation;
 
 import campingplatz.customer.CustomerManagement;
@@ -23,14 +8,6 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
-/**
- * A {@link DataInitializer} implementation that will create dummy data for the
- * application on application startup.
- *
- * @author Paul Henke
- * @author Oliver Gierke
- * @see DataInitializer
- */
 @Component
 @Order(30)
 class ReservationDataInitializer implements DataInitializer {
