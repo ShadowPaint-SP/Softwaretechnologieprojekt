e5a0c7ca16a469cbe9778bc91e2268454a639010
ShadowPaint-SP <71901885+ShadowPaint-SP@users.noreply.github.com>
2023-11-28 21:01:12 +0100

    register+login+footer and small changes

--- src/main/java/campingplatz/CampingPlatz.java
+++ src/main/java/campingplatz/CampingPlatz.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -29,14 +28,12 @@ public class CampingPlatz {
     static class WebSecurityConfiguration {
 
         @Bean
-        SecurityFilterChain videoShopSecurity(HttpSecurity http) throws Exception {
+        SecurityFilterChain CampingPlatzSecurity(HttpSecurity http) throws Exception {
 
-            return http
-                    .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
-                    .csrf(csrf -> csrf.disable())
-                    .formLogin(login -> login.loginProcessingUrl("/login"))
-                    .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"))
-                    .build();
+            http.csrf(csrf -> csrf.disable());
+            http.formLogin(login -> login.loginProcessingUrl("/conf/login"));
+            http.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"));
+            return http.build();
         }
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,6 @@ module.exports = {
     "./src/main/resources/templates/**/*.html"
   ],
   theme: {
-    colors: {
-      'text': 'var(--text)',
-      'background': 'var(--background)',
-      'primary': 'var(--primary)',
-      'secondary': 'var(--secondary)',
-      'accent': 'var(--accent)',
-    },
     fontSize: {
       sm: '0.750rem',
       base: '1rem',
