ba4e6abbe16924d46c0a00bc005a4dd207a16d23
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-01 21:17:48 +0100

    more coverage for SportItem

--- src/test/java/campingplatz/equip/SportItemTest.java
+++ src/test/java/campingplatz/equip/SportItemTest.java
@@ -54,19 +54,40 @@ class SportItemTest {
 		} catch (IllegalArgumentException ignored){
 
 		}
+		try {
+			new SportItem("Ball", Money.of(10,EURO), "");
+			fail("Should have Failed");
+		} catch (IllegalArgumentException ignored){
 
-
-
-
-
+		}
+		try {
+			new SportItem();
+		} catch (Exception e){
+			fail(e.getMessage());
+		}
+		new SportItem();
 	}
+
 	@Test
 	@DisplayName("Test getName()")
 	public void SportItemGetNameTest(){
+		// testet salespoint interne function was unnötig ist
 		String name = "schlumi";
 
 		SportItem sportItem = new SportItem(name, Money.of(10, EURO), "lol");
 
 		assertEquals(name, sportItem.getName());
+	}
+
+	@Test
+	@DisplayName("Test Equals und hashCode Methods")
+	public void SportItemEqualsAndHashCodeTest(){
+		SportItem s1 = new SportItem("Ball1", Money.of(10,EURO), "Bälle");
+
+		SportItem s2 = new SportItem("Ball1", Money.of(10,EURO), "Bälle");
+
+		assertEquals(s1,s2);
+
+		assertEquals(s1.hashCode(),s2.hashCode());
 	}
 }
