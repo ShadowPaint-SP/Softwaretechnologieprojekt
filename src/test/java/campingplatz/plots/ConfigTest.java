30f3262450b11a76edb11959621f95bf26ee03c6
ShadowPaint-SP <71901885+ShadowPaint-SP@users.noreply.github.com>
2023-12-02 23:01:09 +0100

    1

--- src/test/java/campingplatz/plots/ConfigTest.java
+++ src/test/java/campingplatz/plots/ConfigTest.java
@@ -1,8 +1,11 @@
 package campingplatz.plots;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class ConfigTest {
 
 	@Test
