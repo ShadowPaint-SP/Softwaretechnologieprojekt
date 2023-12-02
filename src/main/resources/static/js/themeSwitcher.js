624afaf2e6ae1536b51fcb9ebd5aaef67c5a583e
ShadowPaint-SP <71901885+ShadowPaint-SP@users.noreply.github.com>
2023-12-02 20:44:02 +0100

    theme switcher update

--- src/main/resources/static/js/themeSwitcher.js
+++ src/main/resources/static/js/themeSwitcher.js
@@ -1,14 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const checkbox = document.querySelector('.dark-mode-checkbox');
-	var htmlElement = document.querySelector('html');
+const checkbox = document.querySelector('.dark-mode-checkbox');
 
-	checkbox.addEventListener('change', function () {
+// selected prefered theme
+const userTheme = localStorage.getItem("theme");
 
-		if (this.checked) {
-			htmlElement.setAttribute('data-theme', 'dark');
-		} else {
-			htmlElement.setAttribute('data-theme', 'light');
-		}
-	})
+// theme by the system
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-});
+// check theme initialy
+const themeCheck = () => {
+	if (userTheme === "dark" || (!userTheme && systemTheme)) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		checkbox.setAttribute('checked', 'checked');
+		return;
+	}
+	document.documentElement.setAttribute('data-theme', 'light')
+	checkbox.removeAttribute('checked');
+}
+
+// switch theme manualy
+const themeSwitch = () => {
+	if (document.documentElement.getAttribute('data-theme') === "dark") {
+		document.documentElement.setAttribute('data-theme', 'light')
+		localStorage.setItem("theme", "light");
+		return;
+	}
+	document.documentElement.setAttribute('data-theme', 'dark')
+	localStorage.setItem("theme", "dark");
+}
+
+checkbox.addEventListener('change', () => {
+	themeSwitch();
+})
+
+themeCheck();
