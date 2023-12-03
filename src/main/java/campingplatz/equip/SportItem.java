e51f361ab2218b55a1ad87daa8fd2120e8892972
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-03 23:36:26 +0100

    more Sport...stuff

--- src/main/java/campingplatz/equip/SportItem.java
+++ src/main/java/campingplatz/equip/SportItem.java
@@ -3,20 +3,39 @@ package campingplatz.equip;
 import jakarta.persistence.Entity;
 import lombok.EqualsAndHashCode;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.javamoney.moneta.Money;
 import org.salespointframework.catalog.Product;
+import static org.salespointframework.core.Currencies.EURO;
 
 @Entity
 @EqualsAndHashCode(callSuper = false)
 public class SportItem extends Product {
+	@Getter @Setter
+	private int amount;
+	@Getter @Setter
+	private Money deposit;
+
 
 	@SuppressWarnings({ "unused", "deprecation" })
 	public SportItem() {
+		this.amount = 0;
+		this.deposit = Money.of(0, EURO);
 	}
 
-	public SportItem(String name, Money price, String category) {
+	public SportItem(String name, Money price, Money deposit, String category) {
 		super(name, price);
 		this.addCategory(category);
+		this.amount = 0;
+		this.deposit = deposit;
 	}
+	public SportItem(String name, Money rental_fee, Money deposit, String category, int amount) {
+		super(name, rental_fee);
+		this.addCategory(category);
+		this.amount = amount;
+		this.deposit = deposit;
+	}
+
 
 }
--- src/main/java/campingplatz/equip/SportItemCatalogDataInitializer.java
+++ src/main/java/campingplatz/equip/SportItemCatalogDataInitializer.java
@@ -10,11 +10,17 @@ import static org.salespointframework.core.Currencies.EURO;
 public class SportItemCatalogDataInitializer implements DataInitializer {
 	private SportItemCatalog sportItemCatalog;
 
-	SportItemCatalogDataInitializer(SportItemCatalog sportItemCatalog){
+	SportItemCatalogDataInitializer(SportItemCatalog sportItemCatalog) {
 		this.sportItemCatalog = sportItemCatalog;
 	}
+
 	@Override
 	public void initialize() {
-		sportItemCatalog.save(new SportItem("Ball", Money.of(10,EURO),"Ball"));
+		sportItemCatalog.save(new SportItem("Fußball", Money.of(0.5, EURO), Money.of(16, EURO), "Ball", 10));
+		sportItemCatalog.save(new SportItem("Basketball", Money.of(0.5, EURO), Money.of(20, EURO), "Ball", 8));
+		sportItemCatalog.save(new SportItem("Federballset", Money.of(1, EURO), Money.of(30, EURO), "Federball", 3));
+		sportItemCatalog.save(new SportItem("Tischtennisgarnituren", Money.of(2, EURO), Money.of(100, EURO), "Tischtennis", 2));
+		sportItemCatalog.save(new SportItem("Volleyball", Money.of(0.5, EURO), Money.of(14, EURO), "Volleyball", 6));
+		sportItemCatalog.save(new SportItem("Volleyballnetz", Money.of(2, EURO), Money.of(25, EURO), "Volleyball", 3));
 	}
 }
