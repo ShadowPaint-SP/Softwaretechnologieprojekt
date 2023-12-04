efe3bde35c57dd296f5992be71a348c84e81adcc
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-04 14:43:21 +0100

    new SportItemTest

--- src/main/java/campingplatz/equip/SportItemCatalogDataInitializer.java
+++ src/main/java/campingplatz/equip/SportItemCatalogDataInitializer.java
@@ -19,7 +19,8 @@ public class SportItemCatalogDataInitializer implements DataInitializer {
 		sportItemCatalog.save(new SportItem("Fußball", Money.of(0.5, EURO), Money.of(16, EURO), "Ball", 10));
 		sportItemCatalog.save(new SportItem("Basketball", Money.of(0.5, EURO), Money.of(20, EURO), "Ball", 8));
 		sportItemCatalog.save(new SportItem("Federballset", Money.of(1, EURO), Money.of(30, EURO), "Federball", 3));
-		sportItemCatalog.save(new SportItem("Tischtennisgarnituren", Money.of(2, EURO), Money.of(100, EURO), "Tischtennis", 2));
+		sportItemCatalog.save(new SportItem("Tischtennisgarnituren",
+			Money.of(2, EURO), Money.of(100, EURO), "Tischtennis", 2));
 		sportItemCatalog.save(new SportItem("Volleyball", Money.of(0.5, EURO), Money.of(14, EURO), "Volleyball", 6));
 		sportItemCatalog.save(new SportItem("Volleyballnetz", Money.of(2, EURO), Money.of(25, EURO), "Volleyball", 3));
 	}
