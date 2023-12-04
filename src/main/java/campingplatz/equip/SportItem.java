0e1029d22b62e1f35c9d50186fb8b405ddbe5d08
ShadowPaint-SP <71901885+ShadowPaint-SP@users.noreply.github.com>
2023-12-04 07:52:50 +0100

    removed unused import

--- src/main/java/campingplatz/equip/SportItem.java
+++ src/main/java/campingplatz/equip/SportItem.java
@@ -7,33 +7,34 @@ import lombok.Getter;
 import lombok.Setter;
 import org.javamoney.moneta.Money;
 import org.salespointframework.catalog.Product;
-import static org.salespointframework.core.Currencies.EURO;
 
 @Entity
 @EqualsAndHashCode(callSuper = false)
 public class SportItem extends Product {
-	@Getter @Setter
+	@Getter
+	@Setter
 	private int amount;
-	@Getter @Setter
+	@Getter
+	@Setter
 	private Money deposit;
-
 
 	@SuppressWarnings({ "unused", "deprecation" })
 	public SportItem() {
 	}
-	//rental_fee, price = Leihgebühr | deposit = Kaution
+
+	// rental_fee, price = Leihgebühr | deposit = Kaution
 	public SportItem(String name, Money price, Money deposit, String category) {
 		super(name, price);
 		this.addCategory(category);
 		this.amount = 0;
 		this.deposit = deposit;
 	}
+
 	public SportItem(String name, Money rental_fee, Money deposit, String category, int amount) {
 		super(name, rental_fee);
 		this.addCategory(category);
 		this.amount = amount;
 		this.deposit = deposit;
 	}
-
 
 }
