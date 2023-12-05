1a930f7982e0eb7f2de12c2aaa6d0e872516aafc
idumi3 <idrumeva04@gmail.com>
2023-12-05 14:37:38 +0100

    EndTime und StartTime bei SportItem wegen der Generalisierung gelöscht

--- src/main/java/campingplatz/equip/SportItem.java
+++ src/main/java/campingplatz/equip/SportItem.java
@@ -6,40 +6,31 @@ import lombok.Getter;
 import lombok.Setter;
 import org.javamoney.moneta.Money;
 
-import java.time.LocalTime;
-
 @Getter
 @Entity
 public class SportItem extends DetailedProduct {
 
-	@Getter
+
 	@Setter
 	private int amount;
 
 	@Setter
 	private Money deposit;
 
-	@Getter
-	@Setter
-	private LocalTime startTime;
 
-	@Getter
-	@Setter
-	private LocalTime endTime;
-
-
-	@SuppressWarnings({ "unused", "deprecation" })
+	@SuppressWarnings({ "unused" })
 	public SportItem() {
 	}
 
 	// rental_fee, price = Leihgebühr | deposit = Kaution
+
+
 	public SportItem(String name, Money price, Money deposit, String category) {
 		super(name, price);
 		this.addCategory(category);
 		this.amount = 0;
 		this.deposit = deposit;
 	}
-
 	public SportItem(String name, Money rental_fee, Money deposit, String category, int amount) {
 		super(name, rental_fee);
 		this.addCategory(category);
@@ -47,13 +38,7 @@ public class SportItem extends DetailedProduct {
 		this.deposit = deposit;
 	}
 
-	public SportItem(String name,
-			Money rental_fee,
-			Money deposit,
-			String category,
-			int amount,
-			String image_path,
-			String description) {
+	public SportItem(String name, Money rental_fee, Money deposit, String category, int amount, String image_path, String description) {
 		super(name, rental_fee, image_path, description);
 		this.addCategory(category);
 		this.amount = amount;
--- src/main/java/campingplatz/equip/SportItemCatalogDataInitializer.java
+++ src/main/java/campingplatz/equip/SportItemCatalogDataInitializer.java
@@ -4,8 +4,6 @@ import org.javamoney.moneta.Money;
 import org.salespointframework.core.DataInitializer;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalTime;
-
 import static org.salespointframework.core.Currencies.EURO;
 
 @Component
@@ -19,21 +17,16 @@ public class SportItemCatalogDataInitializer implements DataInitializer {
 	@Override
 	public void initialize() {
 		sportItemCatalog.save(new SportItem("Fußball", Money.of(0.5, EURO),
-				Money.of(16, EURO), "Ball", 10));
+			Money.of(16, EURO), "Ball", 10));
 		sportItemCatalog.save(new SportItem("Basketball", Money.of(0.5, EURO),
-				Money.of(20, EURO), "Ball", 8));
+			Money.of(20, EURO), "Ball", 8));
 		sportItemCatalog.save(new SportItem("Federballset", Money.of(1, EURO),
-				Money.of(30, EURO), "Federball", 3));
+			Money.of(30, EURO), "Federball", 3));
 		sportItemCatalog.save(new SportItem("Tischtennisgarnituren", Money.of(2, EURO),
-				Money.of(100, EURO), "Tischtennis", 2));
+			Money.of(100, EURO), "Tischtennis", 2));
 		sportItemCatalog.save(new SportItem("Volleyball", Money.of(0.5, EURO),
-				Money.of(14, EURO), "Volleyball", 6));
+			Money.of(14, EURO), "Volleyball", 6));
 		sportItemCatalog.save(new SportItem("Volleyballnetz", Money.of(2, EURO),
-				Money.of(25, EURO), "Volleyball", 3));
-
-		sportItemCatalog.findAll().forEach(item -> {
-			item.setStartTime(LocalTime.of(10, 0));
-			item.setEndTime(LocalTime.of(12, 0));
-		});
+			Money.of(25, EURO), "Volleyball", 3));
 	}
 }
