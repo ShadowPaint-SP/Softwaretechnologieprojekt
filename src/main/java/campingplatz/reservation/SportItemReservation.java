3987c19353923c9b8d400329b3109c4b5c1acb13
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-05 22:26:21 +0100

    added SportItemReservation

---
+++ src/main/java/campingplatz/reservation/SportItemReservation.java
@@ -0,0 +1,22 @@
+package campingplatz.reservation;
+
+import campingplatz.equip.SportItem;
+import org.salespointframework.useraccount.UserAccount;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class SportItemReservation extends Reservation<SportItem> {
+	public SportItemReservation() {
+		super();
+	}
+
+	public SportItemReservation(UserAccount userAccount, SportItem sportItem, LocalDateTime arrival, LocalDateTime departure) {
+		super(userAccount, sportItem, arrival, departure);
+	}
+
+	public ChronoUnit getIntervalUnit() {
+		return ChronoUnit.HOURS;
+	}
+
+}
