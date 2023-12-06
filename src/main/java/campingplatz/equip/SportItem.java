1fe3cf255c0b608e033aa8d3b2cdf6394efbc64d
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-07 00:50:14 +0100

    deleted old constructors

--- src/main/java/campingplatz/equip/SportItem.java
+++ src/main/java/campingplatz/equip/SportItem.java
@@ -20,22 +20,6 @@ public class SportItem extends DetailedProduct {
 	}
 
 	// rentalFee, price = Leihgebühr | deposit = Kaution
-
-	//deletion needs new Test
-	public SportItem(String name, Money price, Money deposit, String category) {
-		super(name, price);
-		this.addCategory(category);
-		this.amount = 0;
-		this.deposit = deposit;
-	}
-	//deletion needs new Test
-	public SportItem(String name, Money rentalFee, Money deposit, String category, int amount) {
-		super(name, rentalFee);
-		this.addCategory(category);
-		this.amount = amount;
-		this.deposit = deposit;
-	}
-
 	public SportItem(String name, Money rentalFee, Money deposit, String category, int amount, String imagePath,
 			String description) {
 		super(name, rentalFee, imagePath, description);
