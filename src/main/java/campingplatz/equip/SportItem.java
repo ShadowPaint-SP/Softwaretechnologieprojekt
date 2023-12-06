965dc6bdfe63695bc668119cde4eb7bbd02c2c7a
ShadowPaint-SP <71901885+ShadowPaint-SP@users.noreply.github.com>
2023-12-06 18:58:30 +0100

    main should be running again - removed some dependencys - commented out a test because it is unfinished - removed "unused" because this is not needed

--- src/main/java/campingplatz/equip/SportItem.java
+++ src/main/java/campingplatz/equip/SportItem.java
@@ -10,20 +10,16 @@ import org.javamoney.moneta.Money;
 @Entity
 public class SportItem extends DetailedProduct {
 
-
 	@Setter
 	private int amount;
 
 	@Setter
 	private Money deposit;
 
-
-	@SuppressWarnings({ "unused" })
 	public SportItem() {
 	}
 
 	// rentalFee, price = Leihgebühr | deposit = Kaution
-
 
 	public SportItem(String name, Money price, Money deposit, String category) {
 		super(name, price);
@@ -31,6 +27,7 @@ public class SportItem extends DetailedProduct {
 		this.amount = 0;
 		this.deposit = deposit;
 	}
+
 	public SportItem(String name, Money rentalFee, Money deposit, String category, int amount) {
 		super(name, rentalFee);
 		this.addCategory(category);
@@ -38,7 +35,8 @@ public class SportItem extends DetailedProduct {
 		this.deposit = deposit;
 	}
 
-	public SportItem(String name, Money rentalFee, Money deposit, String category, int amount, String imagePath, String description) {
+	public SportItem(String name, Money rentalFee, Money deposit, String category, int amount, String imagePath,
+			String description) {
 		super(name, rentalFee, imagePath, description);
 		this.addCategory(category);
 		this.amount = amount;
