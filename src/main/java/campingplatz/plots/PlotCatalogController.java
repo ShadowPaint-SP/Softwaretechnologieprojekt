6e56bc5a4dab907080121ece121de4eb7e39f6b3
ShadowPaint-SP <71901885+ShadowPaint-SP@users.noreply.github.com>
2023-12-06 20:04:04 +0100

    end of the day changes - removed empty file - changed some text - reopened route but it has no attributes anymore

--- src/main/java/campingplatz/plots/PlotCatalogController.java
+++ src/main/java/campingplatz/plots/PlotCatalogController.java
@@ -125,16 +125,11 @@ class PlotCatalogController {
         return setupCatalog(model, Optional.ofNullable(user), query, reservationCart);
     }
 
-    /*
-     * @GetMapping("/seasonalplots")
-     * String setupSeasonalCatalog(Model model, @Valid PlotCatalog.SiteState query)
-     * {
-     * var x = plotCatalog.findByType(Plot.PlotType.SEASONAL);
-     * model.addAttribute("allSeasonalPlots", x);
-     * model.addAttribute("searchQuery", query);
-     * return "old/seasonalplotcatalog";
-     * }
-     */
+    @GetMapping("/seasonalplots")
+    String setupSeasonalCatalog(Model model, @Valid PlotCatalog.SiteState query) {
+        model.addAttribute("searchQuery", query);
+        return "old/seasonalplotcatalog";
+    }
 
     @GetMapping("/management/plots")
     String plots(Model model) {
