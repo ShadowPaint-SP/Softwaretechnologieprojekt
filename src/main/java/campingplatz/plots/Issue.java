5087d7c802588e1f1281d18105b7105995e0067e
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-08 19:17:43 +0100

    more detailed Issue

--- src/main/java/campingplatz/plots/Issue.java
+++ src/main/java/campingplatz/plots/Issue.java
@@ -1,17 +1,33 @@
 package campingplatz.plots;
 
+import campingplatz.utils.Priced;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.javamoney.moneta.Money;
 
+import javax.money.MonetaryAmount;
 import java.util.UUID;
 
+@Setter
+@Getter
 @Entity
-public class Issue {
+public class Issue implements Priced {
 
-	@Getter
-	@Setter
 	private @Id UUID id;
 
+	private Money cost;
+
+	private String description;
+
+	public Issue(Money cost, String description) {
+		this.cost = cost;
+		this.description = description;
+	}
+
+	@Override
+	public MonetaryAmount getPrice() {
+		return this.cost;
+	}
 }
