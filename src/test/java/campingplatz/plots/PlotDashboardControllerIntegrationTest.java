46f06dd5638bc551b7a46226add5b2ffad74d610
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-09 23:25:38 +0100

    added Tests for all new functions

--- src/test/java/campingplatz/plots/PlotDashboardControllerIntegrationTest.java
+++ src/test/java/campingplatz/plots/PlotDashboardControllerIntegrationTest.java
@@ -1,11 +1,17 @@
 package campingplatz.plots;
 
+import campingplatz.equip.SportItem;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.util.Streamable;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -41,10 +47,15 @@ class PlotDashboardControllerIntegrationTest {
 	@Test
 	@WithMockUser(username = "meister@mail.de", roles = "EMPLOYEE")
 	void changePlotDetailsTestValidPlotID() throws Exception{
-
+		MvcResult result =mockMvc.perform(get("/management/plots")).andReturn();
+		@SuppressWarnings("unchecked")
+		Streamable<Plot> testList = (Streamable<Plot>) result.getModelAndView().
+			getModelMap().
+			getAttribute("plots");
+		Plot testItem = testList.stream().toList().get(0);
 
 		mockMvc.perform(post("/management/plots/updatePlot")
-				.param("plotID", "lolo")
+				.param("plotID", String.valueOf(testItem.getId()))
 				.param("name", "Ein Plot")
 				.param("size", "30")
 				.param("parkingValue", "1")
@@ -56,10 +67,37 @@ class PlotDashboardControllerIntegrationTest {
 	}
 
 	@Test
-	void createPlotTest() {
+	@WithMockUser(username = "meister@mail.de", roles = "EMPLOYEE")
+	void createPlotTest() throws Exception {
+		mockMvc.perform(post("/management/plots/createPlot")
+				.param("plotID", "lolo")
+				.param("name", "Ein Plot")
+				.param("size", "30")
+				.param("parkingValue", "1")
+				.param("price", "120")
+				.param("picture", "picture")
+				.param("description", "description"))
+			.andExpect(status().isOk());
 	}
 
 	@Test
-	void deletePlotTest() {
+	@WithMockUser(username = "meister@mail.de", roles = "EMPLOYEE")
+	void deletePlotTest() throws Exception {
+		MvcResult result =mockMvc.perform(get("/management/plots")).andReturn();
+		@SuppressWarnings("unchecked")
+		Streamable<Plot> testList = (Streamable<Plot>) result.getModelAndView().
+			getModelMap().
+			getAttribute("plots");
+		Plot testItem = testList.stream().toList().get(0);
+
+		mockMvc.perform(post("/management/plots/deletePlot")
+				.param("plotID", String.valueOf(testItem.getId()))
+				.param("name", "Ein Plot")
+				.param("size", "30")
+				.param("parkingValue", "1")
+				.param("price", "120")
+				.param("picture", "picture")
+				.param("description", "description"))
+			.andExpect(status().isOk());
 	}
 }
