ebb0eb3a2e5d05a0bfff10b7e20c932fac4af0f1
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-09 14:08:00 +0100

    add Tests for TPR01 (incomplete)

--- src/main/java/campingplatz/equip/SportItemReservation.java
+++ src/main/java/campingplatz/equip/SportItemReservation.java
@@ -1,17 +1,22 @@
 package campingplatz.equip;
 
 import campingplatz.reservation.Reservation;
+import jakarta.persistence.Entity;
 import org.salespointframework.useraccount.UserAccount;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+@Entity
 public class SportItemReservation extends Reservation<SportItem> {
 	public SportItemReservation() {
 		super();
 	}
 
-	public SportItemReservation(UserAccount userAccount, SportItem sportItem, LocalDateTime arrival, LocalDateTime departure) {
+	public SportItemReservation(UserAccount userAccount,
+								SportItem sportItem,
+								LocalDateTime arrival,
+								LocalDateTime departure) {
 		super(userAccount, sportItem, arrival, departure);
 	}
 
