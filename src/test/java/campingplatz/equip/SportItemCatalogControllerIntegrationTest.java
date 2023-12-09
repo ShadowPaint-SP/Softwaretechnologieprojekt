1e0a6929a5ddaaf54df77f5658ffc2112993d1ae
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-09 16:12:34 +0100

    added TDA14

--- src/test/java/campingplatz/equip/SportItemCatalogControllerIntegrationTest.java
+++ src/test/java/campingplatz/equip/SportItemCatalogControllerIntegrationTest.java
@@ -28,9 +28,10 @@ class SportItemCatalogControllerIntegrationTest {
 	@WithMockUser(username = "meister@mail.de", roles = "EMPLOYEE")
 	void TDA11() throws Exception {
 		//entspricht TDA11 aus dem Pflichtenheft
-		int money = 12;
+		double money = 12.0;
 
 		MvcResult result =mvc.perform(get("/management/sportsequipment")).andReturn();
+		@SuppressWarnings("unchecked")
 		List<SportItem> testList = (List<SportItem>) result.getModelAndView().
 			getModelMap().
 			getAttribute("items");
@@ -40,7 +41,7 @@ class SportItemCatalogControllerIntegrationTest {
 		mvc.perform(post("/addSportItem")
 			.param("name", testItem.getName())
 			.param("price", String.valueOf(money))
-			.param("deposit", String.valueOf(testItem.getDeposit().getNumber()))
+			.param("deposit", String.valueOf(testItem.getDeposit().getNumber().doubleValue()))
 			.param("amount", String.valueOf(testItem.getAmount()))
 			.param("category", testItem.getCategories().toList().get(0))
 			.param("imagePath", testItem.getImagePath())
@@ -49,13 +50,44 @@ class SportItemCatalogControllerIntegrationTest {
 
 		//check change here
 		MvcResult result1 =mvc.perform(get("/management/sportsequipment")).andReturn();
-		List<SportItem> testList1 = (List<SportItem>) result.getModelAndView().
+		@SuppressWarnings("unchecked")
+		List<SportItem> testList1 = (List<SportItem>) result1.getModelAndView().
 			getModelMap().
 			getAttribute("items");
 		SportItem checkItem = testList.get(0);
 		MonetaryAmount checkMoney = testItem.getPrice();
 		// mist irgendwas fehlt
-		// assertEquals(money, checkMoney.getNumber().intValue());
+		// assertEquals(money, checkMoney.getNumber().doubleValue());
+
+	}
+
+	@Test
+	@WithMockUser(username = "meister@mail.de", roles = "EMPLOYEE")
+	void TDA14() throws Exception {
+		//entspricht TDA14 aus dem Pflichtenheft
+		MvcResult result =mvc.perform(get("/management/sportsequipment")).andReturn();
+		@SuppressWarnings("unchecked")
+		List<SportItem> testList = (List<SportItem>) result.getModelAndView().
+			getModelMap().
+			getAttribute("items");
+		SportItem testItem = testList.get(0);
+		// here one item should be extracted, which can be passed down
+
+		mvc.perform(post("/deleteSportItem")
+				.param("itemName", testItem.getName()))
+			.andExpect(status().is3xxRedirection());
+
+
+		//check change here
+		MvcResult result1 =mvc.perform(get("/management/sportsequipment")).andReturn();
+		@SuppressWarnings("unchecked")
+		List<SportItem> testList1 = (List<SportItem>) result1.getModelAndView().
+			getModelMap().
+			getAttribute("items");
+		assertEquals(testList.size() - 1 , testList1.size());
+		//eigentlich nicht richtig, es könnte ja auch ein anderes Item entfernt worden sein
+		//schlägt auch fehl wenn es keine SportItems gibt.
+
 
 	}
 }
