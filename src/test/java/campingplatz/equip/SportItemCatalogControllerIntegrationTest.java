c359918eb67ed3d47ef27ce556e9b4d91a3bc272
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-09 15:27:45 +0100

    TDA11 complete until one thing

--- src/test/java/campingplatz/equip/SportItemCatalogControllerIntegrationTest.java
+++ src/test/java/campingplatz/equip/SportItemCatalogControllerIntegrationTest.java
@@ -1,14 +1,21 @@
 package campingplatz.equip;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import javax.money.MonetaryAmount;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,27 +28,34 @@ class SportItemCatalogControllerIntegrationTest {
 	@WithMockUser(username = "meister@mail.de", roles = "EMPLOYEE")
 	void TDA11() throws Exception {
 		//entspricht TDA11 aus dem Pflichtenheft
-		mvc.perform(get("/management/sportsequipment"))
-			.andExpect(status().isOk())
-			.andExpect(model().attributeExists("items"));
+		int money = 12;
+
+		MvcResult result =mvc.perform(get("/management/sportsequipment")).andReturn();
+		List<SportItem> testList = (List<SportItem>) result.getModelAndView().
+			getModelMap().
+			getAttribute("items");
+		SportItem testItem = testList.get(0);
 		// here one item should be extracted, which can be passed down
 
 		mvc.perform(post("/addSportItem")
-			.param("name", "Fußball")
-			.param("price", "12")
-			.param("deposit", "30")
-			.param("amount","15")
-			.param("category", "Ball")
-			.param("imagePath", "path")
-			.param("desc", "description"))
+			.param("name", testItem.getName())
+			.param("price", String.valueOf(money))
+			.param("deposit", String.valueOf(testItem.getDeposit().getNumber()))
+			.param("amount", String.valueOf(testItem.getAmount()))
+			.param("category", testItem.getCategories().toList().get(0))
+			.param("imagePath", testItem.getImagePath())
+			.param("desc", testItem.getDesc()))
 			.andExpect(status().is3xxRedirection());
 
 		//check change here
-		mvc.perform(get("/management/sportsequipment"))
-			.andExpect(status().isOk())
-			.andExpect(model().attributeExists("items"));
-
-
+		MvcResult result1 =mvc.perform(get("/management/sportsequipment")).andReturn();
+		List<SportItem> testList1 = (List<SportItem>) result.getModelAndView().
+			getModelMap().
+			getAttribute("items");
+		SportItem checkItem = testList.get(0);
+		MonetaryAmount checkMoney = testItem.getPrice();
+		// mist irgendwas fehlt
+		// assertEquals(money, checkMoney.getNumber().intValue());
 
 	}
 }
