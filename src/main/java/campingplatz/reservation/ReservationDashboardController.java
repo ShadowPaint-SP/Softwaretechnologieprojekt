c5705dcd4267de2f7008ab5a5fcc8cf993b7c85e
vipo180e <vincent.ponizil@mailbox.tu-dresden.de>
2023-12-09 11:09:18 +0100

    fixed /orders

--- src/main/java/campingplatz/reservation/ReservationDashboardController.java
+++ src/main/java/campingplatz/reservation/ReservationDashboardController.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
 import java.util.UUID;
 
 @Controller
+@SessionAttributes("cart")
 public class ReservationDashboardController {
 
 	ReservationRepository<Plot> plotReservations;
