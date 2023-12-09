5fa09c462713cbdadf3bdba33c6c7b0e51a690be
ShadowPaint-SP <71901885+ShadowPaint-SP@users.noreply.github.com>
2023-12-09 16:46:13 +0100

    yea releatively weird progress update for the rest of the guys

--- src/main/java/campingplatz/seasonalplots/SeasonalPlotCatalogController.java
+++ src/main/java/campingplatz/seasonalplots/SeasonalPlotCatalogController.java
@@ -1,6 +1,5 @@
 package campingplatz.seasonalplots;
 
-import campingplatz.plots.Plot;
 import campingplatz.reservation.ReservationRepository;
 import campingplatz.utils.Cart;
 import jakarta.validation.Valid;
@@ -10,9 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -28,9 +29,14 @@ public class SeasonalPlotCatalogController {
 		this.reservationRepository = reservationRepository;
 	}
 
+	@ModelAttribute("cart")
+	Cart<SeasonalPlot> initializeCart() {
+		return new Cart<SeasonalPlot>(SeasonalPlotReservation.class);
+	}
+
 	@GetMapping("/seasonalplotcatalog")
 	String setupSeasonalCatalog(Model model, @LoggedIn Optional<UserAccount> user,
-			@Valid SeasonalPlotCatalog.SeasonalSiteState query, @ModelAttribute("cart") Cart<Plot> reservationCart) {
+			@Valid SeasonalPlotCatalog.SeasonalSiteState query, @ModelAttribute("cart") Cart<SeasonalPlot> reservationCart) {
 		var filteredSeasonalPlots = seasonalPlotCatalog.seasonalFilter(query);
 		var reservedSeasonalPlots = reservationRepository.findPlotsAll();
 		var freeSeasonalPlots = filteredSeasonalPlots.stream().collect(Collectors.partitioningBy(
@@ -44,9 +50,25 @@ public class SeasonalPlotCatalogController {
 
 	@PostMapping("/seasonalplotcatalog/filter")
 	String filter(Model model, @LoggedIn Optional<UserAccount> user, @Valid SeasonalPlotCatalog.SeasonalSiteState query,
-			@ModelAttribute("cart") Cart<Plot> reservationCart) {
+			@ModelAttribute("cart") Cart<SeasonalPlot> reservationCart) {
 		return setupSeasonalCatalog(model, user, query, reservationCart);
 	}
 
-	// TODO Dashboard verweis
+	@PostMapping("/seasonalcheckout/{plot}")
+	String reservate(Model model, @LoggedIn UserAccount userAccount, @PathVariable("plot") SeasonalPlot seasonalPlot,
+			SeasonalPlotReservation.PayMethod payMethod) {
+
+		SeasonalPlotReservation reservation = new SeasonalPlotReservation(userAccount, seasonalPlot,
+				LocalDateTime.now(), null, payMethod);
+		reservationRepository.save(reservation);
+
+		return "redirect:/cart";
+	}
+
+	@GetMapping("/seasonalorders")
+	String orders(Model model, @LoggedIn UserAccount user) {
+		var userReservations = reservationRepository.findByUserId(user.getId());
+		model.addAttribute("ordersCompleted", userReservations);
+		return "servings/orders";
+	}
 }
--- src/main/java/campingplatz/seasonalplots/SeasonalPlotReservation.java
+++ src/main/java/campingplatz/seasonalplots/SeasonalPlotReservation.java
@@ -1,20 +1,25 @@
 package campingplatz.seasonalplots;
 
 import campingplatz.reservation.Reservation;
+import jakarta.persistence.Entity;
+
 import org.salespointframework.useraccount.UserAccount;
 
 import javax.money.MonetaryAmount;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+@Entity
 public class SeasonalPlotReservation extends Reservation<SeasonalPlot> {
 
 	private PayMethod payMethod;
+
 	public SeasonalPlotReservation() {
 		super();
 	}
 
-	public SeasonalPlotReservation(UserAccount user, SeasonalPlot product, LocalDateTime begin, LocalDateTime end, PayMethod payMethod) {
+	public SeasonalPlotReservation(UserAccount user, SeasonalPlot product, LocalDateTime begin, LocalDateTime end,
+			PayMethod payMethod) {
 		super(user, product, begin, end);
 		this.payMethod = payMethod;
 	}
@@ -25,8 +30,9 @@ public class SeasonalPlotReservation extends Reservation<SeasonalPlot> {
 	}
 
 	public MonetaryAmount getTotalPrice(double electricity, double water) {
-		if(PayMethod.MONTHLY.equals(payMethod)){
-			return getProduct().getPrice().add(getProduct().settlementElectricity(electricity).add(getProduct().settlementWater(water)));
+		if (PayMethod.MONTHLY.equals(payMethod)) {
+			return getProduct().getPrice()
+					.add(getProduct().settlementElectricity(electricity).add(getProduct().settlementWater(water)));
 		}
 		return getPrice().add(getProduct().settlementElectricity(electricity).add(getProduct().settlementWater(water)));
 	}
