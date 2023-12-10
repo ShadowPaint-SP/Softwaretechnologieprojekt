401e0a16747e85064352acc4001e26d007753ec4
vipo180e <vincent.ponizil@mailbox.tu-dresden.de>
2023-12-10 20:04:32 +0100

    added arrival and departure to /seasonalplotcatalog

--- src/main/java/campingplatz/seasonalplots/SeasonalPlot.java
+++ src/main/java/campingplatz/seasonalplots/SeasonalPlot.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 import org.javamoney.moneta.Money;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 import javax.money.MonetaryAmount;
 
@@ -20,7 +23,7 @@ public class SeasonalPlot extends Plot {
 	private double waterMeter;
 
 	public SeasonalPlot(String name, Double size, Money price, ParkingLot parking, double electricityMeter,
-						double waterMeter, String imagePath, String description) {	//Price is per Month
+			double waterMeter, String imagePath, String description) { // Price is per Month
 
 		super(name + "\n[Dauercamper]", size, price, parking, imagePath, description);
 
@@ -31,7 +34,8 @@ public class SeasonalPlot extends Plot {
 	public SeasonalPlot() {
 	}
 
-	public MonetaryAmount settlementElectricity(double electricityMeter) { // returns the electricity costs for this time period
+	public MonetaryAmount settlementElectricity(double electricityMeter) { // returns the electricity costs for this
+																			// time period
 		double electricity = electricityMeter - this.electricityMeter;
 		this.electricityMeter = electricityMeter;
 		return Config.getElectricityCosts().multiply(electricity);
@@ -42,4 +46,33 @@ public class SeasonalPlot extends Plot {
 		this.waterMeter = waterMeter;
 		return Config.getWaterCosts().multiply(water);
 	}
+
+	public LocalDateTime getArrival() {
+		// at the first day of next month
+		LocalDateTime arrival = LocalDateTime.now().plusMonths(1).withDayOfMonth(1);
+
+		// in April next year if the season is over
+		if (arrival.getMonthValue() > 10) {
+			arrival = arrival.plusYears(1).withMonth(4);
+		}
+		// in April if the season hasn't started jet
+		else if (arrival.getMonthValue() < 4) {
+			arrival = arrival.withMonth(4);
+		}
+
+		return arrival;
+	}
+	public String getArrivalStr() {
+		return getArrival().toString().substring(0, 10);
+	}
+	
+
+
+	public LocalDateTime getDeparture() {
+		return getArrival().withMonth(10).withDayOfMonth(31);
+	}
+	public String getDepartureStr() {
+		return getDeparture().toString().substring(0, 10);
+	}
+
 }
