4cfe9916bf71af5a5dca91a1aad4179314303ee3
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2023-12-11 22:44:39 +0100

    Issue tests

--- src/main/java/campingplatz/plots/Issue.java
+++ src/main/java/campingplatz/plots/Issue.java
@@ -10,15 +10,15 @@ import org.javamoney.moneta.Money;
 import javax.money.MonetaryAmount;
 import java.util.UUID;
 
-@Setter
+
 @Getter
 @Entity
 public class Issue implements Priced {
 
 	private @Id UUID id;
-
+	@Setter
 	private Money cost;
-
+	@Setter
 	private String description;
 
 	public Issue(Money cost, String description) {
