3a1e9de04de685ddf59ec07553cb895a5bf07403
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2024-01-21 10:33:43 +0100

    refactor

--- src/test/java/campingplatz/accounting/AccountingEntryTest.java
+++ src/test/java/campingplatz/accounting/AccountingEntryTest.java
@@ -3,6 +3,7 @@ package campingplatz.accounting;
 import campingplatz.plots.Plot;
 import campingplatz.seasonalplots.SeasonalPlot;
 import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -10,26 +11,33 @@ import static org.salespointframework.core.Currencies.EURO;
 
 public class AccountingEntryTest {
 
-    @Test
-    public void SeasonalPlotRepairAccountancyEntryConstructorTest() {
-        SeasonalPlot sp = new SeasonalPlot("lol",
+    private Plot dp;
+    private SeasonalPlot dsp;
+
+    @BeforeEach
+    void setUp() {
+        this.dp = new Plot("lol",100.0,
+                Money.of(10,EURO), Plot.ParkingLot.BIKE_PARKING,
+                "","");
+        this.dsp = new SeasonalPlot("lol",
                 100.0,Money.of(10,EURO), Plot.ParkingLot.BIKE_PARKING,
                 100.00,
                 100.00,
                 "",""
-                );
+        );
+    }
+    @Test
+    public void SeasonalPlotRepairAccountancyEntryConstructorTest() {
         assertDoesNotThrow(() -> {
-            new SeasonalPlotRepairAccountancyEntry(10.0, sp);
+            new SeasonalPlotRepairAccountancyEntry(10.0, this.dsp);
         }, "SeasonalPlotRepairAccountancyEntry constructor failed");
     }
 
     @Test
     public void PlotRepairAccountancyEntryConstructorTest() {
-        Plot sp = new Plot("lol",100.0,
-                Money.of(10,EURO), Plot.ParkingLot.BIKE_PARKING,
-                "","");
+
         assertDoesNotThrow(() -> {
-            new PlotRepairAccountancyEntry(10.0, sp);
+            new PlotRepairAccountancyEntry(10.0, this.dp);
         }, "SeasonalPlotRepairAccountancyEntry constructor failed");
     }
 
