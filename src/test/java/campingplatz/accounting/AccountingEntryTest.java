334f76d11fe7aaca50d41ece4c7615423a754e20
Tom Fiedler <tom.fiedler@mailbox.tu-dresden.de>
2024-01-21 10:42:15 +0100

    last one

--- src/test/java/campingplatz/accounting/AccountingEntryTest.java
+++ src/test/java/campingplatz/accounting/AccountingEntryTest.java
@@ -1,21 +1,41 @@
 package campingplatz.accounting;
 
+import campingplatz.customer.Customer;
 import campingplatz.plots.Plot;
+import campingplatz.plots.plotreservations.PlotReservation;
 import campingplatz.seasonalplots.SeasonalPlot;
 import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.salespointframework.useraccount.Password;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountManagement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.salespointframework.core.Currencies.EURO;
 
+@SpringBootTest
 public class AccountingEntryTest {
 
     private Plot dp;
+
+    private PlotReservation dpr;
     private SeasonalPlot dsp;
+
+    private UserAccount customer;
+
+    @Autowired
+    private UserAccountManagement userAccountManagement;
 
     @BeforeEach
     void setUp() {
+        this.customer = userAccountManagement.create("customer", Password.UnencryptedPassword.of("none"),
+                Customer.Roles.CUSTOMER.getValue());
         this.dp = new Plot("lol",100.0,
                 Money.of(10,EURO), Plot.ParkingLot.BIKE_PARKING,
                 "","");
@@ -25,6 +45,14 @@ public class AccountingEntryTest {
                 100.00,
                 "",""
         );
+        this.dpr = new PlotReservation(this.customer, this.dp,
+                LocalDateTime.of(2000,10,1,1,1),
+                LocalDateTime.of(2001,10,1,1,1)
+                );
+    }
+    @AfterEach
+    void tearDown() {
+        userAccountManagement.delete(customer);
     }
     @Test
     public void SeasonalPlotRepairAccountancyEntryConstructorTest() {
@@ -38,7 +66,15 @@ public class AccountingEntryTest {
 
         assertDoesNotThrow(() -> {
             new PlotRepairAccountancyEntry(10.0, this.dp);
-        }, "SeasonalPlotRepairAccountancyEntry constructor failed");
+        }, "PlotRepairAccountancyEntry constructor failed");
+    }
+
+    @Test
+    public void PlotReservationAccountancyEntryConstructorTest() {
+
+        assertDoesNotThrow(() -> {
+            new PlotReservationAccountancyEntry(this.dpr);
+        }, "PlotReservationAccountancyEntry constructor failed");
     }
 
 }
